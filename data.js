fde4a1488a75fe8b521cce13859780af9241bd84
MarkFrid <158204105+MarkFridlin@users.noreply.github.com>
2024-10-07 12:20:25 -0400

    s

---
+++ data.js
@@ -0,0 +1,7 @@
+//part 1
+const students = [
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
+];
